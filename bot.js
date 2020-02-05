29dd8a9aa3bcf62a2de3a7aa11ba7292aa8a694a
pushprajjj <51404724+pushprajjj@users.noreply.github.com>
2020-02-05 16:20:22 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ bot.on('message', message =>{
                 message.channel.send('Aim4Justice is here buddy!') // Sends a message to the channel, with the content
             }
             else{
-                message.channel.send(sender + " I only understand .Aim right now. More will be added when my creator Aim4 JusticeYT is free. :) ")
+                message.channel.send("Hey" sender + " I only understand .Aim right now. More will be added when my creator Aim4 JusticeYT is free. ")
             }
         }
         else{
